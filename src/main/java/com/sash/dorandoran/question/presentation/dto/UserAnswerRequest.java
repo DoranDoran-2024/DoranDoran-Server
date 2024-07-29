35ca3ec0db500acf7c24168a095cd659f1f4cac1
ahnsugyeong <ahnsugyeong@gmail.com>
2024-07-30 04:39:58 +0900

    :memo: Docs: UserAnswerRequest example 작성

--- src/main/java/com/sash/dorandoran/question/presentation/dto/UserAnswerRequest.java
+++ src/main/java/com/sash/dorandoran/question/presentation/dto/UserAnswerRequest.java
@@ -1,11 +1,15 @@
 package com.sash.dorandoran.question.presentation.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UserAnswerRequest {
 
+    @Schema(example = "22")
     private Long questionId;
+
+    @Schema(example = "과학 기술의 발전이 우리의 일상 생활에 미친 영향을 분석해 보세요.")
     private String answer;
 
 }
