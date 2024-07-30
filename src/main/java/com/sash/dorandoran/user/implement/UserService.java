bc729abbf20f8c4bca2c827d32116604d791ee10
ahnsugyeong <ahnsugyeong@gmail.com>
2024-07-30 23:23:28 +0900

    :recycle: Refactor: 불필요한 API 제거

--- src/main/java/com/sash/dorandoran/user/implement/UserService.java
+++ src/main/java/com/sash/dorandoran/user/implement/UserService.java
@@ -1,14 +1,10 @@
 package com.sash.dorandoran.user.implement;
 
-import com.sash.dorandoran.common.exception.GeneralException;
-import com.sash.dorandoran.common.response.status.ErrorStatus;
 import com.sash.dorandoran.jwt.JwtProvider;
-import com.sash.dorandoran.jwt.JwtResponse;
 import com.sash.dorandoran.user.dao.AttendanceRepository;
 import com.sash.dorandoran.user.dao.UserRepository;
-import com.sash.dorandoran.user.domain.*;
-import com.sash.dorandoran.user.presentation.dto.SignInRequest;
-import com.sash.dorandoran.user.presentation.dto.SignUpRequest;
+import com.sash.dorandoran.user.domain.Attendance;
+import com.sash.dorandoran.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,22 +23,6 @@ public class UserService {
     private final AttendanceRepository attendanceRepository;
     private final JwtProvider jwtProvider;
     private final NicknameGenerator nicknameGenerator;
-
-    @Transactional
-    public JwtResponse signUp(SignUpRequest request) {
-        if (userRepository.findByAuthProviderAndEmail(request.getAuthProvider(), request.getEmail()).isPresent()) {
-            throw new GeneralException(ErrorStatus.USERNAME_DUPLICATED);
-        }
-        User user = userRepository.save(buildUser(request));
-        return jwtProvider.generateToken(user);
-    }
-
-    @Transactional
-    public JwtResponse signIn(SignInRequest request) {
-        User user = userRepository.findByAuthProviderAndEmail(request.getAuthProvider(), request.getEmail())
-                .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
-        return jwtProvider.generateToken(user);
-    }
 
     @Transactional
     public void checkAttendance(User user) {
@@ -69,26 +49,6 @@ public class UserService {
         return IntStream.range(0, 7)
                 .mapToObj(i -> attendedDates.contains(startOfWeek.plusDays(i)))
                 .toList();
-    }
-
-    private User buildUser(SignUpRequest request) {
-        String nickname = request.getNickname();
-        if (nickname == null || nickname.trim().isEmpty()) {
-            nickname = nicknameGenerator.generateNickname();
-        }
-
-        return User.builder()
-                .authProvider(request.getAuthProvider())
-                .level(UserLevel.SPROUT)
-                .email(request.getEmail())
-                .nickname(nickname)
-                .role(Role.MEMBER)
-                .build();
-    }
-
-    public User findById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new GeneralException(ErrorStatus.USER_NOT_FOUND));
     }
 
 }
--- src/main/java/com/sash/dorandoran/user/presentation/UserController.java
+++ src/main/java/com/sash/dorandoran/user/presentation/UserController.java
@@ -8,8 +8,6 @@ import com.sash.dorandoran.user.domain.User;
 import com.sash.dorandoran.user.implement.KakaoLoginService;
 import com.sash.dorandoran.user.implement.NaverLoginService;
 import com.sash.dorandoran.user.implement.UserService;
-import com.sash.dorandoran.user.presentation.dto.SignInRequest;
-import com.sash.dorandoran.user.presentation.dto.SignUpRequest;
 import com.sash.dorandoran.user.presentation.dto.UserResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -26,16 +24,6 @@ public class UserController {
     private final UserService userService;
     private final NaverLoginService naverLoginService;
     private final KakaoLoginService kakaoLoginService;
-
-    @PostMapping("/sign-up")
-    public ResponseDto<JwtResponse> signUp(@RequestBody SignUpRequest request) {
-        return ResponseDto.onSuccess(userService.signUp(request));
-    }
-
-    @PostMapping("/sign-in")
-    public ResponseDto<JwtResponse> signIn(@RequestBody SignInRequest request) {
-        return ResponseDto.onSuccess(userService.signIn(request));
-    }
 
     @GetMapping("/info")
     public ResponseDto<UserResponse> getUserInfo(@AuthUser User user) {
