cbddb05f9c8ea7a30e2adde5cc9382c90eb54694
ahnsugyeong <ahnsugyeong@gmail.com>
2024-07-31 18:45:06 +0900

    :arrow_down: Docs: Question API 명세 구체화

--- src/main/java/com/sash/dorandoran/question/presentation/QuestionController.java
+++ src/main/java/com/sash/dorandoran/question/presentation/QuestionController.java
@@ -8,6 +8,7 @@ import com.sash.dorandoran.question.presentation.dto.QuestionResponse;
 import com.sash.dorandoran.question.presentation.dto.UserAnswerRequest;
 import com.sash.dorandoran.question.presentation.dto.UserLevelResponse;
 import com.sash.dorandoran.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +24,19 @@ public class QuestionController {
     private final QuestionService questionService;
     private final UserAnswerService userAnswerService;
 
+    @Operation(
+            summary = "[온보딩] 테스트 질문 4개 생성",
+            description = "온보딩에서 발음 유사성을 측정하기 위한 테스트 질문을 4개 셍성합니다."
+    )
     @GetMapping
     public ResponseDto<List<QuestionResponse>> getRandomQuestions() {
         return ResponseDto.onSuccess(questionService.getQuestions());
     }
 
+    @Operation(
+            summary = "🔑 [온보딩] 테스트 질문 답변 제출",
+            description = "온보딩에서 테스트 질문에 대한 답변을 제출하고, 사용자 레벨을 반환합니다. (SPROUT, STEM, TREE)"
+    )
     @PostMapping("/answers")
     public ResponseDto<UserLevelResponse> submitAnswers(@AuthUser User user, @RequestBody List<UserAnswerRequest> request) {
         return ResponseDto.onSuccess(userAnswerService.calculateScoreAndDetermineLevel(user, request));
