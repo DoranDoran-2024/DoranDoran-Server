1728a4f10ee791bd275d8cabfb16c584cfb48a57
ahnsugyeong <ahnsugyeong@gmail.com>
2024-07-31 18:43:08 +0900

    :memo: Docs: Pronunciation API 명세 구체화

--- src/main/java/com/sash/dorandoran/pronunciation/presentation/PronunciationController.java
+++ src/main/java/com/sash/dorandoran/pronunciation/presentation/PronunciationController.java
@@ -3,6 +3,7 @@ package com.sash.dorandoran.pronunciation.presentation;
 import com.sash.dorandoran.common.response.ResponseDto;
 import com.sash.dorandoran.pronunciation.presentation.dto.PronunciationRequest;
 import com.sash.dorandoran.pronunciation.implement.PronunciationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,10 @@ public class PronunciationController {
 
     private final PronunciationService pronunciationService;
 
+    @Operation(
+            summary = "[온보딩] 발음 유사성 측정",
+            description = "발음 유사성을 측정하여 점수를 반환합니다. 측정 기준은 형태소입니다."
+    )
     @PostMapping("/similarity")
     public ResponseDto<Integer> getSimilarityScore(@RequestBody List<PronunciationRequest> request) {
         return ResponseDto.onSuccess(pronunciationService.getPronunciationSimilarity(request));
